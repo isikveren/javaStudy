d7baf927f19f9d140e19a8c576a2ec2d6bd4bb3a
isikveren <lauxunzi@outlook.com>
2021-08-14 02:57:17 +0800

    update

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world!!");
         //test1 test1 = new test1();
         test1.IsSon();
     }
